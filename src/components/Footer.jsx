72cf5f455adf0ee2d1708ce87aeb9aeb599b073a
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-06-08 02:10:29 +0530

    My Portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import React from "react";
 
 const Footer = () => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
-    <Box w="100%" p={4} color="white" textAlign="center" borderTopColor="grey" border="1" marginTop={1}>
+    <Box
+      w="100%"
+      p={4}
+      color={isDark ? "white" : "black"}
+      textAlign="center"
+      borderTopColor="grey"
+      border="1"
+      marginTop={1}
+    >
       Made with ❤️ by Muralikumar
     </Box>
   );
