c6d6c4acf7680a8013bf47c479f282bf972a214e
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-07-05 12:35:32 +0530

    updated projects

--- src/Util/constants.js
+++ src/Util/constants.js
@@ -75,7 +75,7 @@ export const projects = [
       "Integrated OpenWeatherMap API to retrieve real-time weather data for various locations.",
     ],
     techStack: ["React JS", "Open API"],
-    github: "", // Add the GitHub link here
+    github: "", // Add the GitHub link 
     liveLink: "https://weather-app-vite-alpha.vercel.app/", // Add the live link here
   },
   {
