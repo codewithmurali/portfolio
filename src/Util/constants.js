2ddc9a2522a85ced560df4420042e53f2b321f25
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-07-14 15:41:19 +0530

    updated projects

--- src/Util/constants.js
+++ src/Util/constants.js
@@ -66,8 +66,21 @@ export const projects = [
     techStack: ["React JS"],
     github: "https://github.com/yourusername/decra-project", 
     liveLink: "https://decra-project.netlify.app/", 
-  },  {
+  }, 
+   {
     name: "Health-Remainder",
+    image: decra, 
+    description: [
+      "Developed a comprehensive website for my department's national symposium using React.js.",
+      "Created and maintained a user-friendly, responsive interface to ensure optimal user experience.",
+      "Implemented various features and enhancements to meet the symposium's requirements."
+    ],
+    techStack: ["HTML", "CSS", "JS", "Node JS"],
+    github: "https://github.com/yourusername/decra-project", 
+    liveLink: "https://decra-project.netlify.app/", 
+  },
+  {
+    name: "",
     image: decra, 
     description: [
       "Developed a comprehensive website for my department's national symposium using React.js.",
